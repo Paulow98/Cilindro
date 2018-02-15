25ec0b872b0f1e780141d93e5754a971a3d166ae
Paulow98 <paulow98@hotmail.com>
2018-02-15 11:33:43 +0000

    Created the volume function

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,14 +8,20 @@ namespace Cilindro
 {
     class Program
     {
+        const float PI = 3.1415926f;
         static void Main(string[] args)
         {
             float a, r;
-            const float PI = 3.1415926f;
+            
             Console.Write("Digite a altura do cilindro:");
             a = Convert.ToSingle(Console.ReadLine());
             Console.Write("\nDigite o raio do cilindro: ");
             r = Convert.ToSingle(Console.ReadLine());
+        }
+
+        float volume(float altura, float raio)
+        {
+            return PI * (float)Math.Pow(raio,2) * altura;
         }
     }
 }
